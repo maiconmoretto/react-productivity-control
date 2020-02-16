2122d089199938c2137a2fda92797d8dffd5e82f
maiconmoretto <maiconmorettos@gmail.com>
2020-02-16 17:58:43 -0300

    Create project whith list os tasks

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import "./App.css";
+import Task from "./Task/Task";
+
+class App extends Component {
+  state = {
+    tasks: [
+      { id: 1, dueDate: "01/01/01", number: 123 },
+      { id: 2, dueDate: "02/02/02", number: 234 },
+      { id: 3, dueDate: "03/03/03", number: 345 }
+    ],
+    otherState: "some other value",
+    showTasks: true
+  };
+
+  render() {
+    let tasks = null;
+
+    if (this.state.showTasks) {
+      tasks = (
+        <div>
+          {this.state.tasks.map((task, index) => {
+            return <Task dueDate={task.dueDate} number={task.number} />;
+          })}
+        </div>
+      );
+    }
+
+    return (
+      <div className="App">
+        {tasks}
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Task/Task.js
@@ -0,0 +1,27 @@
+  
+  import React from "react";
+  // import './task.css';
+  
+  const task = (props) => {
+    return <p onClick={props.click}> due date: {props.dueDate}, number: {props.number} </p>;
+  //   return (
+  //     <div className="Task">
+  //       <p onClick={props.click}> i am {props.name}, and have {props.age} age</p>
+  //       <input type="text" onChange={props.changed} value={props.name}/>
+  //       <p>
+  //         Task number: <input type="number" name="taskNumber" />
+  //       </p>
+  
+  //       <p>
+  //         Due Date: <input type="date" name="date" />
+  //       </p>
+  
+  //       <p>
+  //         Date Finished: <input type="date" name="dateFinished" />
+  //       </p>
+  //     </div>
+  //     )
+  };
+  
+  export default task;
+  
