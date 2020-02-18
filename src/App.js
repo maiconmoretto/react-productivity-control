7e40111248954b32226cc67f3554f40ffd890312
maiconmoretto <maiconmorettos@gmail.com>
2020-02-18 15:19:37 -0300

    Changed way of list

--- src/App.js
+++ src/App.js
@@ -1,68 +1,25 @@
-import React, { Component } from "react";
-import "./App.css";
-import Task from "./Task/Task";
+import React, { useState } from "react";
+import TaskTable from "./Task/TaskTable";
 
-class App extends Component {
-  state = {
-    tasks: [
-      { id: 1, dueDate: "01/01/01", number: 123 },
-      { id: 2, dueDate: "02/02/02", number: 234 },
-      { id: 3, dueDate: "03/03/03", number: 345 }
-    ],
-    otherState: "some other value",
-    showTasks: true
-  };
+const App = () => {
+  const tasksData = [
+    { id: 1, dueDate: "01/01/01", number: 123 },
+    { id: 2, dueDate: "02/02/02", number: 234 },
+    { id: 3, dueDate: "03/03/03", number: 345 }
+  ];
 
-  numberChangehandler = (event, id) => {
-    const taskIndex = this.state.tasks.findIndex(p => {
-      return p.id === id;
-    });
+  const [tasks, setTasks] = useState(tasksData);
 
-    const task = {
-      ...this.state.tasks[taskIndex]
-    };
+  return (
+    <div className="App">
+      <h1>List of tasks:</h1>
 
-    task.number = event.target.value;
-
-    const tasks = [...this.state.tasks];
-    tasks[taskIndex] = task;
-
-    this.setState({ tasks: tasks });
-  }
-
-  deleteTaskHandler = taskIndex => {
-    const tasks = this.state.tasks.slice();
-    tasks.splice(taskIndex, 1);
-    this.setState({ tasks: tasks });
-  };
-
-  render() {
-    let tasks = null;
-
-    if (this.state.showTasks) {
-      tasks = (
-        <div>
-          {
-            this.state.tasks.map((task, index) => {
-              return <Task key={task.id}
-              dueDate={task.dueDate} 
-              number={task.number} 
-              click={() => this.deleteTaskHandler(index)}
-              changed={(event)  => this.numberChangehandler(event, task.id)}
-              />;
-            })
-          }
-        </div>
-      );
-    }
-
-    return (
-      <div className="App">
-        <h1>List of tasks:</h1>
-        {tasks}
+      <div className="flex-large">
+        <h2>View Tasks</h2>
+        <TaskTable tasks={tasks}/>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default App;
---
+++ src/Task/TaskTable.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+const TaskTable = props => (
+  <table>
+    <thead>
+      <tr>
+        <th>Number</th>
+        <th>Due date</th>
+        <th>Finished date</th>
+        <th>Actions</th>
+      </tr>
+    </thead>
+    <tbody>
+      {props.tasks.length > 0 ? (
+        props.tasks.map(task => (
+          <tr key={task.id}>
+            <td>{task.number}</td>
+            <td>{task.dueDate}</td>
+            <td>{task.finishedDate}</td>
+            <td>
+              <button className="button muted-button">Edit</button>
+              <button className="button muted-button">Delete</button>
+            </td>
+          </tr>
+        ))
+      ) : (
+        <tr>
+          <td colSpan={3}>No tasks</td>
+        </tr>
+      )}
+    </tbody>
+  </table>
+);
+
+export default TaskTable;
