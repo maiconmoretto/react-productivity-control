ab60f13152da7c6c0ab5e45ecc6b5158c4ce249a
maiconmoretto <maiconmorettos@gmail.com>
2020-02-22 17:02:31 -0300

    Added style to add task form

--- src/Forms/AddTaskForm.js
+++ src/Forms/AddTaskForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import './AddTaskForm.css'
 
 const AddTaskForm = props => {
   const initialFormState = { id: null, number: '', dueDate: '', finishedDate: '', diffDays: 0 }
@@ -22,15 +23,23 @@ const AddTaskForm = props => {
         setTask(initialFormState)
       }}
     >
-      <label>Number</label>
+      <label className="label">Number</label>
+      <br></br>
       <input type="text" name="number" value={task.number} onChange={handleInputChange} />
+      <br></br>
       <label>Due date</label>
+      <br></br>
       <input type="text" name="dueDate" value={task.dueDate} onChange={handleInputChange} />
+      <br></br>
       <label>Finished date</label>
+      <br></br>
       <input type="text" name="finishedDate" value={task.finishedDate} onChange={handleInputChange} />
+      <br></br>
       <label>Diff in Days</label>
+      <br></br>
       <input type="text" name="diffDays" value={task.diffDays} onChange={handleInputChange} />
-      <button>Add new task</button>
+      <br></br>
+      <input type="submit" value="Save"/>
     </form>
   )
 }
